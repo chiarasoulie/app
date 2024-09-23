d8e915e97a3ec0289a40d052ef24ce8f2db79b4a
csoul00 <souliechiara@gmail.com>
2024-09-23 18:00:03 +0200

    fin tp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/firstapplicationchiara/MainActivity.kt
+++ app/src/main/java/com/example/firstapplicationchiara/MainActivity.kt
@@ -1,55 +1,28 @@
 package com.example.firstapplicationchiara
 
-import android.content.Context
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.window.core.layout.WindowHeightSizeClass
-import androidx.window.core.layout.WindowSizeClass
-import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.firstapplicationchiara.ui.theme.FirstApplicationChiaraTheme
 import kotlinx.serialization.Serializable
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 @Serializable class DestProfil
@@ -63,23 +36,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
             val navController = rememberNavController()
+            val backstack by navController.currentBackStackEntryAsState()
+            val currentDestination = backstack?.destination
 
             FirstApplicationChiaraTheme {
-                Scaffold(bottomBar = {
-                    NavigationBar {
-                        val currentDestination = navController.currentBackStackEntry?.destination
-                        NavigationBarItem(
-                            icon = {  Icon(Icons.Rounded.AccountCircle, contentDescription ="menu") }, label = { Text("Mon profil") },
-                            selected = currentDestination?.hasRoute<DestProfil>() == true,
-                            onClick = { navController.navigate(DestProfil()) })
-                        NavigationBarItem(
-                            icon = { Icon(Icons.Rounded.Menu, contentDescription ="menu")}, label = { Text("Films") },
-                            selected = currentDestination?.hasRoute<DestFilms>() == true,
-                            onClick = { navController.navigate(DestFilms()) })
-                    }
+                Scaffold(
+                    bottomBar = {
+                        if (currentDestination?.hasRoute<DestProfil>() == false) {
+
+                            NavigationBar {
+
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+
+                                            Icons.Rounded.AccountCircle,
+                                            contentDescription = "menu"
+                                        )
+                                    }, label = { Text("Mon profil") },
+                                    selected = currentDestination?.hasRoute<DestProfil>() == true,
+                                    onClick = { navController.navigate(DestProfil()) })
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+                                            Icons.Rounded.Menu,
+                                            contentDescription = "menu"
+                                        )
+                                    }, label = { Text("Films") },
+                                    selected = currentDestination?.hasRoute<DestFilms>() == true,
+                                    onClick = { navController.navigate(DestFilms()) })
+                            }
+                        }
                 })  { innerPadding ->
                     NavHost(navController = navController, startDestination = DestProfil(), modifier = Modifier.padding(innerPadding)) {
-                        composable<DestProfil> { Greeting(name = "app", classes = windowSizeClass,navController = navController ) }
+                        composable<DestProfil> {
+                            Greeting(
+                                name = "app",
+                                classes = windowSizeClass,
+                                navController = navController
+                            )
+                        }
                         composable<DestFilms> { DescFilms() }
 
                     }
